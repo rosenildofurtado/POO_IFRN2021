400d40243513bc86ee924d9aefaf3c7b8610a18a
Rosenildo Furtado <robala-sat@hotmail.com>
2021-02-01 21:40:10 +0000

    Criou a pasta da lista de revisão.

---
+++ Lista Revisao/exRevQ1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass{
+  public static viod Main(string[] args){
+    string estado;
+    int hab;
+    double area;
+    Console.WriteLine("Digite o nome do estado:");
+    estado = Console.ReadLine();
+    Console.WriteLine("Digite o número de habitantes:");
+    hab = int.parse(Console.ReadLine();
+    Console.WriteLine("Digite a área do estado em km2");
+    area = double.parse(Console.ReadLine());
+  }
+}
